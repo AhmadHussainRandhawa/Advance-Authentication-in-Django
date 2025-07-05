e16c4f597240800a43b35f88710ead5e3695f091
Ahmad Hussain <official.ahmadrandhawa@gmail.com>
2025-07-05 22:40:12 +0500

    Add User Registration form and email verification token generator

---
+++ core_auth/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegistrationForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput)
+    password2 = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data.get("password1") != cleaned_data.get("password2"):
+            raise forms.ValidationError("Passwords do not match")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.is_active = False  # Will activate after email click
+        if commit:
+            user.save()
+        return user
